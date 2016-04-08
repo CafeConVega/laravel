4c0761e6977efaccede5742b2a65856bc4300462
CafeConVega <joshua.vega.r@gmail.com>
2016-04-07 22:23:06 -0400

    added nfldb models
    models with protected names

---
+++ app/agg_play.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class agg_play extends Model
+{
+    protected $table = 'agg_play';
+}
---
+++ app/play_player.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class play_player extends Model
+{
+    protected $table = 'play_player';
+}
