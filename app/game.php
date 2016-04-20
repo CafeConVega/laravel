f7a5ddf0e2eeb028c52294596d0c1ecc90eebd49
CafeConVega <joshua.vega.r@gmail.com>
2016-04-20 17:51:02 -0400

    game App fix

--- app/game.php
+++ app/game.php
@@ -9,22 +9,22 @@ class game extends Model
     protected $table = 'game';
     protected $primaryKey = 'gsis_id';
     public function home_team() {
-        return $this->belongsTo('app\team', 'home_team', 'team_id');
+        return $this->belongsTo('App\team', 'home_team', 'team_id');
     }
     
     public function away_team() {
-        return $this->belongsTo('app\team', 'away_team', 'team_id');
+        return $this->belongsTo('App\team', 'away_team', 'team_id');
     }
     
     public function drive() {
-        return $this->hasMany('app\drive', 'gsis_id', 'gsis_id');
+        return $this->hasMany('App\drive', 'gsis_id', 'gsis_id');
     }
     
     public function play() {
-        return $this->hasMany('app\play', 'gsis_id', 'gsis_id');
+        return $this->hasMany('App\play', 'gsis_id', 'gsis_id');
     }
     
     public function play_player() {
-        return $this->hasMany('app\play_player', 'gsis_id', 'gsis_id');
+        return $this->hasMany('App\play_player', 'gsis_id', 'gsis_id');
     }
 }
