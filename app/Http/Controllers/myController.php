63f870788423b9e9e2be6a7e5a2d50f841cef730
CafeConVega <joshua.vega.r@gmail.com>
2016-04-25 12:43:13 -0400

    final players test3

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -152,13 +152,13 @@ class myController extends Controller
             $play_players[] = $obj;
         }
         
-            foreach($players_id->player as $player) {
-            $obj = new \stdClass;
-            $obj = $player;
-            $players[] = $obj;
-        }
+//        foreach($players_id->player as $player) {
+//            $obj = new \stdClass;
+//            $obj = $player;
+//            $players[] = $obj;
+//        }
         
-        $alldata =  ["game" =>$game_data, "drives" =>$drives, "plays" =>$plays, "play_player" => $play_players, "players" => $players];
+        $alldata =  ["game" =>$game_data, "drives" =>$drives, "plays" =>$plays, "play_player" => $play_players, "players" => $game_id];
         return response()->json($alldata);
     }
 
