dd79fb85c9cdc5a339111e60f867ed51caa769f9
CafeConVega <joshua.vega.r@gmail.com>
2016-05-03 23:17:53 -0400

    fixed data and added away button

--- resources/views/select_game.blade.php
+++ resources/views/select_game.blade.php
@@ -3,20 +3,29 @@
 @section('content')
 <div class="container">       
 <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Home Team
+  <button id="home_team" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Home Team
   <span class="caret"></span></button>
   <ul class="dropdown-menu">
        @foreach($teams as $teamss)
-            <li data = ".{{ $teamss->team_id}}." ><a>{{ $teamss->city." ".$teamss->name}}</a></li>
+            <li data = "{{ $teamss->team_short}}" ><a>{{ $teamss->city." ".$teamss->name}}</a></li>
        @endforeach
   </ul>
 </div>
 <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Year
+  <button id="year" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Year
   <span class="caret"></span></button>
   <ul class="dropdown-menu">
        @foreach($years as $yearss)
-            <li data = ".{{ $yearss->season_year}}." ><a>{{ $yearss->season_year }}</a></li>
+            <li data = "{{$yearss->season_year}}" ><a>{{ $yearss->season_year }}</a></li>
+       @endforeach
+  </ul>
+</div>
+<div class="dropdown">
+  <button id="away_team" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Away Team
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+       @foreach($teams as $teamss)
+            <li data = "{{ $teamss->team_short}}" ><a>{{ $teamss->city." ".$teamss->name}}</a></li>
        @endforeach
   </ul>
 </div>
