5bbcbbee18dc2281bed45f40329ce21a9256e19d
CafeConVega <joshua.vega.r@gmail.com>
2016-05-04 17:50:02 -0400

    ob short last

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -66,7 +66,7 @@ public function newTeam(Request $request) {
  }
     
 public function buttonData() {
-        $teams = team_plus::orderBy('short')->all();
+        $teams = team_plus::all()->orderBy('short');
         $years = game::select('season_year')->orderBy('season_year')->distinct()->get();
         $all_data = ["teams" => $teams, "years" => $years];
         return view('gamerecap', $all_data);
