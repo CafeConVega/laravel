fa1abdee15fffa25090e63253d196ef67e5f233c
CafeConVega <joshua.vega.r@gmail.com>
2016-05-03 23:11:20 -0400

    list for years

--- resources/views/select_game.blade.php
+++ resources/views/select_game.blade.php
@@ -3,11 +3,20 @@
 @section('content')
 <div class="container">       
 <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Team
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Home Team
   <span class="caret"></span></button>
   <ul class="dropdown-menu">
        @foreach($teams as $teamss)
             <li data = ".{{ $teamss->team_id}}." ><a>{{ $teamss->city." ".$teamss->name}}</a></li>
+       @endforeach
+  </ul>
+</div>
+<div class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Year
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+       @foreach($years as $yearss)
+            <li data = ".{{ $years->season_year}}." ><a>{{ $yearss->season_year}}</a></li>
        @endforeach
   </ul>
 </div>
